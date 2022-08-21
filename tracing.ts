a1519ce4b2cf56360cb2c37216a9cd6c0db9a962
Tasin Ishmam <tasinishmam@gmail.com>
2022-08-21 14:47:30 +0600

    Add Jaeger

--- tracing.ts
+++ tracing.ts
@@ -1,14 +1,11 @@
-// tracing.ts
-
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { SimpleSpanProcessor, ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { trace, Tracer } from "@opentelemetry/api";
+import { JaegerExporter } from "@opentelemetry/exporter-jaeger";
 
 export default function initializeTracing(serviceName: string): Tracer {
-
-
     const provider = new NodeTracerProvider({
         resource: new Resource({
             [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
@@ -16,6 +13,11 @@ export default function initializeTracing(serviceName: string): Tracer {
     });
 
     const consoleExporter = new ConsoleSpanExporter()
+    const jaegerExporter = new JaegerExporter({
+        endpoint: "http://localhost:14268/api/traces",
+    });
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
     provider.addSpanProcessor(new SimpleSpanProcessor(consoleExporter));
 
     provider.register();
