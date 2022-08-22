20cd5d212a57497d9d6b44a9d01cbb5fdc9a3a7b
Tasin Ishmam <tasinishmam@gmail.com>
2022-08-22 12:32:57 +0600

    Remove console exporter

--- tracing.ts
+++ tracing.ts
@@ -1,6 +1,6 @@
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { SimpleSpanProcessor, ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { trace, Tracer } from "@opentelemetry/api";
 import { JaegerExporter } from "@opentelemetry/exporter-jaeger";
@@ -16,12 +16,10 @@ export default function initializeTracing(serviceName: string): Tracer {
         }),
     });
 
-    const consoleExporter = new ConsoleSpanExporter()
     const jaegerExporter = new JaegerExporter({
         endpoint: "http://localhost:14268/api/traces",
     });
 
-    provider.addSpanProcessor(new SimpleSpanProcessor(consoleExporter));
     provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
 
     registerInstrumentations({
