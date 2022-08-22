5fa0e909b5c55cf30f58c8b22f9044913fedaaac
Tasin Ishmam <tasinishmam@gmail.com>
2022-08-22 16:30:38 +0600

    Add BatchSpanProcessor and Probability Sampling for production

--- tracing.ts
+++ tracing.ts
@@ -1,6 +1,6 @@
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { SimpleSpanProcessor, BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { trace, Tracer } from "@opentelemetry/api";
 import { JaegerExporter } from "@opentelemetry/exporter-jaeger";
@@ -8,9 +8,14 @@ import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { PrismaInstrumentation } from '@prisma/instrumentation';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express'
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+import { TraceIdRatioBasedSampler } from '@opentelemetry/core'
 
 export default function initializeTracing(serviceName: string): Tracer {
+
+    const traceRatio = process.env.NODE_ENV === 'production' ? 0.1 : 1.0;
+
     const provider = new NodeTracerProvider({
+        sampler: new TraceIdRatioBasedSampler(traceRatio),
         resource: new Resource({
             [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
         }),
@@ -20,7 +25,11 @@ export default function initializeTracing(serviceName: string): Tracer {
         endpoint: "http://localhost:14268/api/traces",
     });
 
-    provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+    if (process.env.NODE_ENV === 'production') {
+        provider.addSpanProcessor(new BatchSpanProcessor(jaegerExporter))
+    } else {
+        provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter))
+    }
 
     registerInstrumentations({
         instrumentations: [
