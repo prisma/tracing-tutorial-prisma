883e24db278e1604d2d02394c69bc38b7a5756cb
Tasin Ishmam <tasinishmam@gmail.com>
2022-08-27 02:40:04 +0600

    Update migrations

--- prisma/migrations/20220826203628_init/migration.sql
+++ prisma/migrations/20220826203628_init/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
-CREATE TABLE "post" (
+CREATE TABLE "Post" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "description" TEXT,
     "userId" TEXT NOT NULL,
-    CONSTRAINT "post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "user" (
+CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL
 );
